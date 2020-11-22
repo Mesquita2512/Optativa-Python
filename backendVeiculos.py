44e08e3ad2f71d95fdb835fdc534c711f8881016
Mesquita2512 <54009128+Mesquita2512@users.noreply.github.com>
2020-11-22 10:20:38 -0300

    novas altereções

--- backendVeiculos.py
+++ backendVeiculos.py
@@ -1,17 +1,27 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
-from veiculo import Veiculo
-import os
-app = Flask(__name__)
-caminho = os.path.dirname(os.path.abspath(__file__))
-arquivobd = os.path.join(caminho, 'veiculos.bd')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + arquivobd
-app.config['SQLAlCHEMY_TRACK_MODIFICATIOS'] = False
-db = SQLAlchemy(app)
+import config
+import modelo
 
 
-
-@app.route("/")
+@config.app.route("/")
 def padrao():
-    return " backend operante"
-app.run()
+    return "bem vindo ao backend"
+
+@config.app.route("/listar_veiculos")
+def listar_veiculos():
+    veiculos = config.db.session.query(modelo.Veiculo).all()
+    retorno = []    
+    for p in veiculos:
+        retorno.append(p.json())
+    resposta = config.jsonify(retorno)
+    resposta.headers.add("Access-Control-Allow-Origin", "*")
+    return resposta 
+
+@config.app.route("/incluir_planta", methods=['post'])
+def incluir_veiculo():
+    dados = config.request.get_json()
+    novoVeiculo = modelo.Veiculo(**dados)
+    config.db.session.add(novoVeiculo)
+    config.db.session.commit()
+    return {"resultado":'ok'}
+
+config.app.run(debug=True)
--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
-import os
-app = Flask(__name__)
-caminho = os.path.dirname(os.path.abspath(__file__))
-arquivobd = os.path.join(caminho, 'veiculos.bd')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + arquivobd
-app.config['SQLAlCHEMY_TRACK_MODIFICATIOS'] = False
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy 
+import os 
+app = Flask(__name__) 
+caminho = os.path.dirname(os.path.abspath(__file__)) 
+arquivobd = os.path.join(caminho, "veiculos.db") 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+arquivobd 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 db = SQLAlchemy(app)
