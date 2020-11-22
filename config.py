f61506f97cb72c906609d288888c04be539d212d
Mesquita2512 <54009128+Mesquita2512@users.noreply.github.com>
2020-11-21 23:05:07 -0300

    delete

--- config.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
-import os
-app = Flask(__name__)
-caminho = os.path.dirname(os.path.abspath(__file__))
-arquivobd = os.path.join(caminho, 'veiculos.bd')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + arquivobd
-app.config['SQLAlCHEMY_TRACK_MODIFICATIOS'] = False
-db = SQLAlchemy(app)
