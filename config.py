f8be071d060962bdea576ee5b71ae6fe28600214
Mesquita2512 <54009128+Mesquita2512@users.noreply.github.com>
2020-11-22 10:36:50 -0300

    k
    k

--- config.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy 
-import os 
-app = Flask(__name__) 
-caminho = os.path.dirname(os.path.abspath(__file__)) 
-arquivobd = os.path.join(caminho, "veiculos.db") 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+arquivobd 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
-db = SQLAlchemy(app)
